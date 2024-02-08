c4a260506a97e7502addfa5cd6f70fd92a9ab683
Samuel Alison <samuelalison13@gmail.com>
2024-02-08 13:59:24 +0100

    first commit

---
+++ src/components/Main.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Main = ({children}) => {
+  return (
+    <main>{children}</main>
+  )
+}
---
+++ src/components/Progress.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Progress({ index, numberOfQuestions, points, maxPossiblePoints, answer }) {
+  return (
+    <header className='progress'>
+        <progress max={numberOfQuestions} value={index + Number(answer !== null)}/>
+        <p>Question <strong>{index + 1}</strong>/{numberOfQuestions} </p>
+        <p><strong>{points}</strong> / {maxPossiblePoints}</p>
+        </header>
+  )
+}
